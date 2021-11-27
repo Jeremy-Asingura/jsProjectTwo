dda3424c55e772034aedb5658317a94f056dd930
Jeremy Asingura <blessedjosh234@gmail.com>
2021-11-27 10:51:01 +0300

    added stuff

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let usersAge = prompt("How old are you?");
 
-  if (usersAge % 2 == 0){
-    alert("Your age is even!!");
+  if (usersAge % 2 === 0){
+    alert("Your age is an even number!!");
 } 
 
-  else if (usersAge % 2 == 1){
+  else if (usersAge % 2 === 1){
   alert("Your age is odd!!");
 } 
 
    else{
-   alert("Please give real information");
+   alert("Please give valid input next time");
 }
